384da6d1903fdd33725c2133d950d10115fb67c4
teesha2201 <131008053+teesha2201@users.noreply.github.com>
2023-11-24 02:16:18 +0530

    Add files via upload

---
+++ src/Component/WatchTitan.js
@@ -0,0 +1,67 @@
+import React,{useEffect,useState} from "react";
+import '../Style/Laptop.css'
+import {Outlet,NavLink} from "react-router-dom";
+import AddFooter from "../Component/Footer";
+import {useDispatch} from "react-redux";
+import {addtoCart} from "../Redux/Slice";
+
+const Titan = ()=>{
+    const [watch,setWatch] =useState([])
+    const dispatch = useDispatch();
+    useEffect(()=>{
+      async function apiFun(){
+        const fetchdata = await fetch("https://new-ecommerce-backend-m62a.onrender.com/api/getdatafromproductstore")
+        const res = await fetchdata.json();
+        console.log(res);
+      
+        setWatch(res);
+         }
+         apiFun();
+     },[])
+    return(
+        <>
+           <div className='laptopsuperContainer'>
+                <div className="laptopcart-wrapper">
+            {watch.filter((item)=>item.category==="electronics" && item.subCategory ==="titanWatch").map((item)=>{
+              const {
+                id = item.id,
+                image = item.image,
+                price= parseInt(item.price),
+               
+              }= item;
+                return (
+                    <div className="laptopimg-wrapper item" key={item.id}>
+                      <NavLink to={`/moreDetails/${item.id}`}>
+                      <img src={item.image} alt="Not Found"/>
+                      </NavLink>
+                       <br/>
+                    <div className="laptoptext-wrapper item">
+                        <span className='Brand'>    
+                        </span>
+                        <span className='ProductName'>
+                          {item.product_name.slice(0,30)}
+                        </span>
+                        <span></span>
+                        <h3>
+                           Rs.{item.price}
+                        </h3>
+                        <div className="laptopbtn-wrapper item ">
+                        <NavLink to={`/addtoCart/${item.id}`}>
+                          <button onClick={()=>dispatch(addtoCart({id,image,price}))} className='laptopaddtocartbtn'>Add To Cart</button>
+                        </NavLink>  
+                       
+                        </div>
+                    </div>
+                   
+                    </div>
+                       
+                    )
+                  })}
+            </div>
+      </div>
+      <Outlet/>
+      <AddFooter/>
+        </>
+    )
+}
+export default Titan
