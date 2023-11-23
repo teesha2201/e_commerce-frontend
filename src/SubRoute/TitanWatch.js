5189309cfcb3a19acb390431bccdd5a0fef6ec7b
teesha2201 <131008053+teesha2201@users.noreply.github.com>
2023-11-23 12:21:13 +0530

    Add files via upload

--- src/SubRoute/TitanWatch.js
+++ src/SubRoute/TitanWatch.js
@@ -41,7 +41,7 @@ const Titan = ()=>{
                         <NavLink to={`/addtoCart/${item.id}`}>
                           <button onClick={()=>{}} className='laptopaddtocartbtn'>Add To Cart</button>
                         </NavLink>  
-                          <button onClick={()=>{}} className='laptopremoveitembtn'>Remove item </button>
+                          <button onClick={()=>{}} className='laptopremoveitembtn'>Buy Now </button>
                         </div>
                     </div>
                    
